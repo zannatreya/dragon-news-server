3d6c0649084f6961c67fe04285bc557ed122f09c
zannatreya <zannatulbintabahar@yahoo.com>
2023-01-21 15:52:51 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.get("/", (req, res) => {
+  res.send("News API running!");
+});
+
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log("Dragon News Server running on port", port);
+});
